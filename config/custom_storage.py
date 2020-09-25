9d81bd9d7cb558d5ac72a6f374a6277a09329801
이은덕 <pleed0215@ieundeog-ui-iMac.local>
2020-09-25 16:41:28 +0900

    bb

---
+++ config/custom_storage.py
@@ -0,0 +1,8 @@
+from storages.backends.s3boto3 import S3Boto3Storage
+
+class StaticStorage(S3Boto3Storage):
+    location = "static/"
+    file_overwrite = False
+
+class UploadsStorage(S3Boto3Storage):
+    location = "uploads/"
